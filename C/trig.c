6139a338e8161501c9c15546909fc371576fa9dc
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2022-04-20 11:33:25 -0300

    trig functions in c

--- C/trig.c
+++ C/trig.c
@@ -1,15 +1,54 @@
 #include<stdio.h>
 #include<math.h>
 
-int main(void){
-    printf(" sin(x)     | cos(x)     | tan(x)     | \n");
-    int theta=45;
-    for(theta;theta<=90;theta=theta+5){
-        double res_sin,res_cos,res_tan;
-        res_sin=sin(theta);
-        res_cos=cos(theta);
-        res_tan=tan(theta);
-        printf("%d | %d | %d | \n",res_sin,res_cos,res_tan);
 
+float DegToRad(float angle);
+int trig_sin();
+int trig_cos();
+int trig_tan();
+
+int main(void){
+    printf("  |  2|  N| hexa\n");
+    printf(" N|  2|  N| hexa\n");
+    printf("-----+-----+ -----\n");
+    trig_sin();
+    trig_cos();
+    trig_tan(); 
+    printf("-----+-----+ -----\n");
+}
+
+float DegToRad(float angle){
+    float res=0.0174532925*angle;
+    return res;
+}
+int trig_sin(){
+    int theta=45;
+    printf("========================\n");
+    printf("sin(x)\n");
+    for(theta;theta<=90;theta=theta+5){
+        float res_sin;
+        res_sin=sinf(DegToRad(theta));
+        printf("%d | %2d | %3d | %4d |%x\n",theta,res_sin,res_sin,res_sin,res_sin);
+    }
+}
+int trig_cos(){
+    int theta=45;
+    printf("========================\n");
+    printf("cos(x)\n");
+    for(theta;theta<=90;theta=theta+5){
+        float res_cos;
+        res_cos=cosf(DegToRad(theta));
+        printf("%d | %2d | %3d | %4d |%x\n",theta,res_cos,res_cos,res_cos,res_cos);
+    }
+}
+int trig_tan(){
+    int theta=45;
+    printf("========================\n");
+    printf("tan(x)\n");
+    for(theta;theta<=90;theta=theta+5){
+        float res_tan;
+        res_tan=tanf(DegToRad(theta));
+        
+        printf("%d | %2d | %3d | %4d |%x\n",theta,res_tan,res_tan,res_tan,res_tan);
     }
 }
