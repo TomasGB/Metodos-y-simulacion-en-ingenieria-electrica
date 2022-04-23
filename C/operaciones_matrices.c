218bdb0ef5b05af05be22ebcd39dd768431ccacb
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2022-04-23 00:50:17 -0300

    asd

--- C/operaciones_matrices.c
+++ C/operaciones_matrices.c
@@ -29,7 +29,7 @@ int translate3D(int Fig[3][11], int Tx , int Ty, int Tz, int TFig[3][11]);
 int scale2D(int Fig[2][11], float Sx , float Sy, int TFig[2][11]);
 int scale3D(int Fig[3][11], float Sx , float Sy, float Sz, int TFig[3][11]);
 
-//float mat_mult(float mat_1[2][11], int mat_2[2][11], int res[11][2]);
+float mat_mult(float mat_1[2][2], int mat_2[2][11], float res[11][2]);
 float rotation2D(int Fig[2][11], int angle,float res[11][2]);
 
 int main(){
@@ -42,7 +42,8 @@ int main(){
 
     float ang_rad=(45/180)*PI;
 
-    float matrizDeRotacion[2][2]={{cos(ang_rad),-sin(ang_rad)},{sin(ang_rad),cos(ang_rad)}};
+    //float matrizDeRotacion[2][2]={{cos(ang_rad),-1*sin(ang_rad)},{sin(ang_rad),cos(ang_rad)}};
+    float matrizDeRotacion[2][2]={{1,0},{0,1}};
     int Fig[2][11]={{-6, -6, -7, 0, 7, 6, 6, -3, -3, 0, 0},{ -7, 2, 1, 8, 1, 2, -7, -7, -2, -2, -7}};
     int TFig[2][11],TFig2[2][11];
 
@@ -53,15 +54,18 @@ int main(){
     float Sx=3;
     float Sy=0.5;
 
-    float resultado[11][2];
+    float resultado[11][2]={{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
 
     mostrarMatrizInt(Fig);
-    translate2D(Fig, Tx , Ty, TFig);
-    scale2D(TFig, Sx , Sy, TFig);
-    printf("\n");
-    mostrarMatrizInt(TFig);
-    //rotation2D(Fig, angulo,resultado);
+    //translate2D(Fig, Tx , Ty, TFig);
+    //scale2D(TFig, Sx , Sy, TFig);
     //printf("\n");
+    //mostrarMatrizInt(TFig);
+    //rotation2D(Fig, angulo,resultado);
+    printf("\n");
+
+    mat_mult(matrizDeRotacion,Fig,resultado);
+    mostrarMatrizFloat(resultado);
     //mostrarMatrizFloat(resultado);
 
 }
@@ -111,17 +115,14 @@ float mostrarMatrizFloat(float mat_1[11][2]){
 
 float mat_mult(float mat_1[2][2], int mat_2[2][11], float res[11][2]){
     for(int i=0;i<2;i++){
-            for(int j=0;j<11;j++){
-                res[i][j]=0;
-                for(int k=0;k<2;k++){
-                    printf("%0.4f ",(mat_1[i][k]*mat_2[k][j]));
-                    res[i][j]=res[i][j]+(mat_1[i][k]*mat_2[k][j]);
-                }
-                printf("\n");
-            }  
-            printf("\n");
+        for(int j=0;j<11;j++){
+            res[i][j]=0;
+        for(int k=0;k<2;k++){
+            res[i][j]+=mat_1[i][k]*mat_2[k][j];
         }
-        return res[11][2];
+    }
+}
+    return res[11][2];
 }
 
 //============== Transformaciones ======================
@@ -178,15 +179,15 @@ int scale3D(int Fig[3][11], float Sx , float Sy, float Sz, int TFig[3][11]){
 
 float rotation2D(int Fig[2][11], int angle,float result[11][2]){
     float ang_rad=(angle/180)*PI;
-    //float matrizDeRotacion[2][2]={{cos(ang_rad),(-1)*sin(ang_rad)},{sin(ang_rad),cos(ang_rad)}};
-    float matrizDeRotacion[2][2]={{1,1},{1,1}};
+    float matrizDeRotacion[2][2]={{cos(ang_rad),(-1)*sin(ang_rad)},{sin(ang_rad),cos(ang_rad)}};
+    //float matrizDeRotacion[2][2]={{1,1},{1,1}};
     
     for(int i=0;i<2;i++){
         for(int j=0;j<11;j++){
             result[i][j]=0;
             for(int k=0;k<2;k++){
                 //acum=acum+(matrizDeRotacion[i][k]*Fig[k][j]);
-                result[i][j]=result[i][j]+(matrizDeRotacion[i][k]*Fig[k][j]);
+                result[i][j]+=matrizDeRotacion[i][k]*Fig[k][j];
                 //result[i][j]=acum;
             }
         }
