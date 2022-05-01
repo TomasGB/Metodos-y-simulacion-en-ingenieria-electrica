8c5d3db6828cefd9fad85147377396f39baf9042
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2022-04-30 22:18:03 -0300

    transformaciones final

--- C/operaciones_matrices.c
+++ C/operaciones_matrices.c
@@ -4,59 +4,30 @@
 
 #define PI 3.14159265358979323846
 
-
-typedef struct {
-    char operacion;
-    double parametros[3];
-}instruccion;
-
-double mostrarMatriz(int dimensiones, double mat_1[3][11]);
-double transform(int dimensiones, double Fig3D[3][11],char operacion, double parametros[3], double res[3][11]);
-double translate(int dimensiones, double Fig[3][11], float Tx , float Ty, float Tz, double TFig[3][11]);
-double scale(int dimensiones,double Fig[3][11], float Sx , float Sy, float Sz, double TFig[3][11]);
+double showMatrix(int dimension, double Fig[3][11]);
+double transform(int dimension, double Fig[3][11],char operation, double parameters[3], double res[3][11]);
+double translate(int dimension, double Fig[3][11], float Tx , float Ty, float Tz, double TFig[3][11]);
+double scale(int dimension,double Fig[3][11], float Sx , float Sy, float Sz, double TFig[3][11]);
 double rotation2D(double Fig[3][11], float angle,double res[3][11]);
 double rotation3D(double Fig[3][11], float angle_x, float angle_y, float angle_z,double res[3][11]);
+int calculateDimension(void);
+double loadMatrix(int dimension, double Fig[3][11]);
+void saveInFile(int dimension,double Fig[3][11]);
 
-//double transform2(int filas, double Fig3D[3][11], float Tx , float Ty, float Tz, float Sx , float Sy, float Sz, int angle_x, int angle_y, int angle_z, double TFig3D[3][11], double res3D[3][11]);
-//int contarNumeroDeColumnas(void);
-int contarNumeroDeDimensiones(void);
-double cargarMatriz(int dimensiones, double matriz[3][11]);
-void GuardarEnArchivo(int dimensiones,double arreglo[3][11]);
-instruccion readTransformations(instruccion transformaciones);
 
 int main(){
 
-    //float ang_rad=(PI/180)*90;
+    int dimensiones=calculateDimension();
+    double Fig[3][11];
 
-    int dimensiones=contarNumeroDeDimensiones();
-    double Fig3D[3][11];
-
-    Fig3D[3][11]=cargarMatriz(dimensiones,Fig3D);
-    double TFig3D[3][11]={{0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0},{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-    //double res3D[3][11]={{0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0},{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-
-    //double Fig3D[3][11]={{-6, -6, -7, 0, 7, 6, 6, -3, -3, 0, 0},{ -7, 2, 1, 8, 1, 2, -7, -7, -2, -2, -7},{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-    /*
-    //parametros de transformacion
-    int angulo = 0;
-    int angulo_x= 0;
-    int angulo_y = 0;
-    int angulo_z = 0;
-
-    float Tx = 0;
-    float Ty = 0;
-    float Tz = 1;
-
-    float Sx = 1;
-    float Sy = 1;
-    float Sz = 1;
-    */
+    Fig[3][11]=loadMatrix(dimensiones,Fig);
+    double TFig[3][11]={{0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0},{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
    // Transformaciones 3D
     printf("Dimensiones: %d\n",dimensiones);
     printf("\n");
     printf("Figura original:\n");
-    mostrarMatriz(dimensiones,Fig3D);
+    showMatrix(dimensiones,Fig);
 
     //lee instrucciones del archivo
     char operacion;
@@ -84,18 +55,17 @@ int main(){
         it++;
 
         if(it==3 && operacion!='r'){
-            transform(dimensiones, Fig3D,operacion,parametros,Fig3D);
-            Fig3D[3][11]=Fig3D[3][11];
+            transform(dimensiones, Fig,operacion,parametros,Fig);
+            Fig[3][11]=Fig[3][11];
             it=0;
             jk=0;
         }
         if(it==3 && operacion=='r'){
-            transform(dimensiones, Fig3D,operacion,parametros,TFig3D);
-            Fig3D[3][11]=TFig3D[3][11];
+            transform(dimensiones, Fig,operacion,parametros,TFig);
+            Fig[3][11]=TFig[3][11];
             it=0;
             jk=0;
         }
-
     }
 
     fclose(fp);
@@ -103,82 +73,68 @@ int main(){
     printf("\n");
     printf("Figura transformada:\n");
 
-    mostrarMatriz(dimensiones,Fig3D);
-
-    GuardarEnArchivo(dimensiones,Fig3D);
-    
-
+    showMatrix(dimensiones,Fig);
+    saveInFile(dimensiones,Fig);
 }
 
 //========= Print Matrices ==========================
 
-double mostrarMatriz(int dimensiones, double mat_1[3][11]){
-    for (int it_x=0;it_x<dimensiones;it_x++){
+double showMatrix(int dimension, double Fig[3][11]){
+    for (int it_x=0;it_x<dimension;it_x++){
         for (int it_y=0;it_y<11;it_y++){
-            printf("%0.4f ",mat_1[it_x][it_y]);
+            printf("%0.4f ",Fig[it_x][it_y]);
         }
         printf("\n");
     }
 }
 //============== Transformaciones ======================
 
-double transform(int dimensiones, double Fig3D[3][11],char operacion, double parametros[3], double res[3][11]){
+double transform(int dimension, double Fig3D[3][11], char operation, double parameters[3], double res[3][11]){
 
-    switch (operacion){
+    switch (operation){
         case 't':
-            if(dimensiones==3){
-                translate(dimensiones,Fig3D,parametros[0],parametros[1],parametros[2], res);
-            }else if(dimensiones==2){
-                translate(dimensiones,Fig3D,parametros[0],parametros[1],0,res);
+            if(dimension==3){
+                translate(dimension,Fig3D,parameters[0],parameters[1],parameters[2], res);
+            }else if(dimension==2){
+                translate(dimension,Fig3D,parameters[0],parameters[1],0,res);
             }
         break;
 
         case 's':
-        if(dimensiones==3){
-                scale(dimensiones,Fig3D,parametros[0],parametros[1],parametros[2], res);
-            }else if(dimensiones==2){
-                scale(dimensiones,Fig3D,parametros[0],parametros[1],0, res);
+        if(dimension==3){
+                scale(dimension,Fig3D,parameters[0],parameters[1],parameters[2], res);
+            }else if(dimension==2){
+                scale(dimension,Fig3D,parameters[0],parameters[1],0, res);
             }
         break;
         case 'r':
-        if(dimensiones==3){
-                rotation3D(Fig3D,parametros[0],parametros[1],parametros[2],res);
-            }else if(dimensiones==2){
-                rotation2D(Fig3D,parametros[2],res);
+        if(dimension==3){
+                rotation3D(Fig3D,parameters[0],parameters[1],parameters[2],res);
+            }else if(dimension==2){
+                rotation2D(Fig3D,parameters[2],res);
             }
         break;
     }
 }
 
-double transform2(int dimensiones, double Fig3D[3][11], float Tx , float Ty, float Tz, float Sx , float Sy, float Sz, int angle_x, int angle_y, int angle_z, double TFig3D[3][11], double res3D[3][11]){
-    translate(dimensiones, Fig3D, Tx , Ty, Tz, TFig3D);
-    scale(dimensiones, TFig3D, Sx , Sy, Sz, TFig3D);
-
-    if (dimensiones==2){
-        rotation2D(TFig3D, angle_z, res3D);
-    }else{
-        rotation3D(TFig3D, angle_x,angle_y,angle_z, res3D);
-    }
-    return res3D[3][11];
-}
-double translate(int dimensiones, double Fig[3][11], float Tx , float Ty, float Tz, double TFig[3][11]){
+double translate(int dimension, double Fig[3][11], float Tx , float Ty, float Tz, double TFig[3][11]){
     for (int i=0;i<11;i++){
         TFig[0][i]=Fig[0][i]+Tx; 
         TFig[1][i]=Fig[1][i]+Ty;
 
-        if(dimensiones==2){
+        if(dimension==2){
             }else{
             TFig[2][i]=Fig[2][i]+Tz;
         }
     }
     return TFig[3][11];
 }
-double scale(int dimensiones,double Fig[3][11], float Sx , float Sy, float Sz, double TFig[3][11]){
+double scale(int dimension,double Fig[3][11], float Sx , float Sy, float Sz, double TFig[3][11]){
     for (int i=0;i<11;i++){
         TFig[0][i]=Fig[0][i]*Sx; 
         TFig[1][i]=Fig[1][i]*Sy;
 
-        if(dimensiones==2){
+        if(dimension==2){
 
         }else{
             TFig[2][i]=Fig[2][i]*Sz;
@@ -248,9 +204,9 @@ double rotation3D(double Fig[3][11], float angle_x, float angle_y, float angle_z
 
 //========================= Archivos =====================================================
 
-int contarNumeroDeDimensiones(void){
+int calculateDimension(void){
     FILE * fp;
-    fp = fopen("fig_original3D.txt","r");
+    fp = fopen("fig_original.txt","r");
 
     if(!fp){
         printf("no se encontro el archivo\n");
@@ -272,9 +228,8 @@ int contarNumeroDeDimensiones(void){
     return filas;
 
 }
-/*
-int contarNumeroDeColumnas(void){
-    
+
+double loadMatrix(int dimension, double matrix[3][11]){
     FILE * fp;
     fp = fopen("fig_original.txt","r");
 
@@ -283,93 +238,32 @@ int contarNumeroDeColumnas(void){
         exit(101);
     }
 
-    int columnas,i=0;
-
-    while(feof(fp)!='\0'){
-        columnas+=1;
-    }
-    
-    fclose(fp);
-
-    return columnas;
-}
-*/
-
-double cargarMatriz(int dimensiones, double matriz[3][11]){
-    FILE * fp;
-    fp = fopen("fig_original3D.txt","r");
-
-    if(!fp){
-        printf("no se encontro el archivo\n");
-        exit(101);
-    }
-
-    for (int i=0;i<dimensiones;i++){
+    for (int i=0;i<dimension;i++){
         for (int j=0;j<11;j++){
-            fscanf(fp,"%lf ",&matriz[i][j]);
+            fscanf(fp,"%lf ",&matrix[i][j]);
         }
     }
-    if(dimensiones==2){
+    if(dimension==2){
         for (int j=0;j<11;j++){
-            matriz[2][j]=0;
+            matrix[2][j]=0;
         }
     }
     fclose(fp);
 
-    return matriz[3][11];
+    return matrix[3][11];
 
 }
 
-void GuardarEnArchivo(int dimensiones,double arreglo[3][11]){
+void saveInFile(int dimension,double Fig[3][11]){
     FILE *fp;
 
-    fp = fopen( "test.txt", "w+" );
+    fp = fopen( "fig_transformada.txt", "w+" );
 
-    for( int i = 0; i < dimensiones ; i++) {
+    for( int i = 0; i < dimension ; i++) {
         for( int j= 0; j < 11; j++) {
-            fprintf(fp, "%.4f ",arreglo[i][j]);
+            fprintf(fp, "%.4f ",Fig[i][j]);
         }
         fprintf(fp, "\n");
     }
     fclose( fp );
 }
-
-
-
-/*
- void readTransformations(void){
-        ejemplo de instruccion:
-
-        transformacion, eje x, eje y, eje z
-
-        traslacion: t , escalado: s, rotacion: r
-
-        t,15,-2,6 ----> traslada 15 unidades en x, -2 en y, 6 en z
-        r,5,0,45 ----> rota 5° en x, 0° en y, 45° en z
-
-
-
-    FILE * fp;
-    fp = fopen("transformaciones.txt","r");
-
-    if(!fp){
-        printf("no se encontro el archivo\n");
-        exit(101);
-    }
-
-    int jk=0;
-    int it=0;
-    char a;
-    while(!feof(fp))
-    {
-        if(jk<1){
-            operacion=fgetc(fp);
-            jk++;
-        }
-        fscanf(fp,"%lf",&parametros[it]);
-
-        it++;
-
-    }
-}
-*/
